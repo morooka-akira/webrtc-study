55f372b75f3a6869271a1c3dcb6901ff32f160de
Heiya <morooka.akira.inon29@gmail.com>
2020-01-28 17:52:56 +0900

    update.

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
-const MEDIA_CONFIG = { video: true, audio: true }
-
 /**
- * UserMediaからストリームを取得する(Video&Audio)
+ * 3. UserMediaからストリームを取得する(Video&Audio)
  */
 async function setupUserMedia(): Promise<MediaStream | undefined> {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia(MEDIA_CONFIG)
+    // NOTE: Streamから取得できる情報を設定で指定できます(今回は動画と音声)
+    const mediaConfig = { video: true, audio: true }
+    const stream = await navigator.mediaDevices.getUserMedia(mediaConfig)
+    // NOTE: videoTracksから端末がどのデバイスと接続しているかの情報が取得できます
+    const videoTracks = stream.getVideoTracks()
+    console.log(`Using video device: ${videoTracks[0].label}`)
     return stream
   } catch (err) {
-    console.log('An error occured! ' + err)
+    console.log('[error: setupUserMedia] ', err)
   }
 }
 
 /**
- * videoタグにStreamを流し表示する
+ * 4. videoタグにStreamを流し表示する
  * @param stream MediaStream
  */
 function showVideo(stream: MediaStream) {
   const video = document.getElementById('local-video') as HTMLVideoElement
-  const videoTracks = stream.getVideoTracks()
-  console.log(`Using video device: ${videoTracks[0].label}`)
+  // NOTE: videoElementのsrcObjectにstreamを接続すると動画が再生できるようになる
   video.srcObject = stream
 }
 
 /**
- * 初期セットアップ
+ * 2. ストリームを取得して、Videoタグを再生する
  */
 async function handleShowVideo() {
   // start video capture
@@ -34,6 +36,9 @@ async function handleShowVideo() {
   }
 }
 
+/**
+ * 1.ボタンが押された時のハンドラー
+ */
 const btnShowVideo = document.getElementById('btn-show-video')
 if (btnShowVideo) {
   btnShowVideo.addEventListener(
