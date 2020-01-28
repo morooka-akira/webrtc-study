996d626220b226eafda0957b0022fd1caa45e82c
Heiya <morooka.akira.inon29@gmail.com>
2020-01-28 11:22:20 +0900

    Change to wss.

--- server/signaling.ts
+++ server/signaling.ts
@@ -1,15 +1,26 @@
 import WebSocket from 'ws'
+import fs from 'fs'
+import https from 'https'
 
-const wss = new WebSocket.Server({ port: 5001 })
+try {
+  const server = https.createServer({
+    cert: fs.readFileSync('./localhost.pem'),
+    key: fs.readFileSync('./localhost-key.pem'),
+  })
+  const wss = new WebSocket.Server({ server })
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message)
-    wss.clients.forEach(function(client) {
-      // NOTE: 自分以外の接続にブロードキャスト
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message)
-      }
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      console.log('received: %s', message)
+      wss.clients.forEach(function(client) {
+        // NOTE: 自分以外の接続にブロードキャスト
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message)
+        }
+      })
     })
   })
-})
+  server.listen(5001)
+} catch (e) {
+  console.error(e)
+}
